41203057f6f23864dd633f3acd78c765910e65a5
mikhail <mixa199525@yandex.ru>
2024-12-03 17:47:44 +0000

    форма

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pleasure_ui
+
+![Фото формы регистрации также с правой стороны есть анимация](./ru/pleasure_ui/photo/screen_1.png)
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,9 +7,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulseScale: 'pulseScale 2s ease-in-out infinite',
+      },
+      keyframes: {
+        pulseScale: {
+          '0%, 100%': { transform: 'scale(1)', opacity: 0.7 },
+          '50%': { transform: 'scale(1.1)', opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [daisyui],
-}
+};
+
+
 
 
